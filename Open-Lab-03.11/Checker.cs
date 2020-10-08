03fbaa131ac7dbccf4a446ec9dcbbfa7e1b441da
MilosM33 <milosmarejka89@gmail.com>
2020-10-08 14:37:15 +0200

    init

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,12 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_03._11
 {
     public class Checker
     {
-        public bool IsPalindrome(string str)
-        {
-            throw new NotImplementedException();
-        } 
+        public bool IsPalindrome(string str) => str == new string(str.Reverse().ToArray());
+        
     }
 }
